5ba7c2188d26ad0eb3dda924c0e0ec7e97bd3b97
amichaiman <amichai@mantinband.com>
2018-02-25 18:38:10 +0200

    question answered for songs

--- final_q2/library.c
+++ final_q2/library.c
@@ -17,6 +17,7 @@ void destroy_library(library *l)
 
 void create_library(library *l)
 {
+    l->num_of_songs = 0;
     l->head = NULL;
 }
 
@@ -30,7 +31,7 @@ int add_song(library *l, song *s)
         new_node->next = NULL;
 
         l->head = new_node;
-
+        l->num_of_songs++;
         return 1;
     }
 
@@ -43,7 +44,7 @@ int add_song(library *l, song *s)
         new_node->next = l->head;
 
         l->head = new_node;
-
+        l->num_of_songs++;
         return 1;
     }
 
@@ -67,18 +68,19 @@ int song_appears(library *l, song *to_find)
 
 int remove_song(library *l, song *s)
 {
-    Node *cur_node = l->head;
-    Node *to_delete;
+    Node *cur_node;
+    Node *to_delete = NULL;
 
+    cur_node = l->head;
     while (cur_node->next != NULL)
     {
-        if (strcmp(cur_node->next->my_song->name,s->name))
+        if (strcmp(cur_node->next->my_song->name,s->name) == NULL)
         {
-            to_delete = cur_node->next;
             cur_node->next = to_delete->next;
 
             free(to_delete->my_song);
             free(to_delete);
+            l->num_of_songs--;
             return 1;
         }
         cur_node = cur_node->next;
@@ -87,6 +89,29 @@ int remove_song(library *l, song *s)
     return 0;
 }
 
+
+
+
+song *get_song(library *l, char *name) {
+    Node *cur_node = l->head;
+
+    while (cur_node != NULL)
+    {
+        if (strcmp(cur_node->my_song->name,name) == 0)
+        {
+            return cur_node->my_song;
+        }
+    }
+    return NULL;
+}
+
+int get_num_of_songs_in_library(library *l)
+{
+    return l->num_of_songs;
+}
+
+void print_songs(library *l, void *print_by, void(*print_songs_by_type)(void *))
+{}
 
 song *copy_song(song *s1)
 {
