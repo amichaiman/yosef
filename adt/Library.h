ab2a6942c6b381f41ef12b0943404d05f41c0d54
amichaiman <amichai@mantinband.com>
2018-02-25 19:07:49 +0200

    adt

--- adt/Library.h
+++ adt/Library.h
@@ -7,8 +7,10 @@
 typedef struct Library library;
 
 void create_library(library *l);
-void print_songs(library *l, void *print_by, void(*print_object)(void *));
+void destroy_library(library *l);
 int add_song(library *l, void *object, void *(*copy_object)(void *object),int (*object_appears)(library *, void *object));
 int remove_song(library *l, void *object, int (*cmp_objects)(void *o1,void *o2));
+void print_songs(library *l, void *print_by, void(*print_object)(void *));
+int get_num_of_songs_in_library(library *l);
 
 #endif
